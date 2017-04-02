b202dc02fa2d220f1ed2c9c7e38d029974794278
ibrahim abdullah <ibrahim.a.abdullah@gmail.com>
2017-04-02 23:53:16 +0000

    Done.

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -1,31 +1,31 @@
 def selects_all_female_bears_return_name_and_age
-  "Write your SQL query here"
+  "SELECT BEARS.NAME, BEARS.AGE FROM BEARS WHERE GENDER = 'F';"
 end
 
 def selects_all_bears_names_and_orders_in_alphabetical_order
-  "Write your SQL query here"
+  "SELECT BEARS.NAME FROM BEARS ORDER BY BEARS.NAME ASC;"
 end
 
 def selects_all_bears_names_and_ages_that_are_alive_and_order_youngest_to_oldest
-  "Write your SQL query here"
+  "SELECT BEARS.NAME, BEARS.AGE FROM BEARS WHERE ALIVE = 1 ORDER BY BEARS.AGE ASC;"
 end
 
 def selects_oldest_bear_and_returns_name_and_age
-  "Write your SQL query here"
+  "SELECT BEARS.NAME, BEARS.AGE FROM BEARS ORDER BY BEARS.AGE DESC LIMIT 1;"
 end
 
 def select_youngest_bear_and_returns_name_and_age
-  "Write your SQL query here"
+  "SELECT BEARS.NAME, BEARS.AGE FROM BEARS ORDER BY BEARS.AGE ASC LIMIT 1;"
 end
 
 def selects_most_prominent_color_and_returns_with_count
-  "Write your SQL query here"
+  "SELECT BEARS.COLOR, COUNT(BEARS.COLOR) FROM BEARS GROUP BY BEARS.COLOR ORDER BY COUNT (*) DESC LIMIT 1;"
 end
 
 def counts_number_of_bears_with_goofy_temperaments
-  "Write your SQL query here"
+  "SELECT COUNT(BEARS.TEMPERAMENT) FROM BEARS WHERE BEARS.TEMPERAMENT='goofy';"
 end
 
 def selects_bear_that_killed_Tim
-  "Write your SQL query here"
+  "SELECT * FROM BEARS WHERE BEARS.NAME IS NULL;"
 end
